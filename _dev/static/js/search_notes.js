f253ef9854fa6f0db978714b7d15c22c6bbed7cf
Keto Zhang <keto.zhang@gmail.com>
2020-05-11 03:56:06 -0700

    Add notes/index.html search bar

--- _dev/static/js/search_notes.js
+++ _dev/static/js/search_notes.js
@@ -22,6 +22,15 @@ function search_notes() {
     result_box.empty()
     results.forEach(function (result, id) {
         var page = result.item
-        result_box.append(`<li> <a href="${page.url}">${page.title}</a> </li>`)
+        url_as_nav = page.url.slice(1, -1).replace(/\//g, ' > ');
+        result_box.append(
+            `<a id="search_result_${id}" class="list-group-item list-group-item-action" href="${page.url}"><strong>${page.title}</strong> <small class="text-muted">${url_as_nav}</small></a>`
+        )
+        if (page.content !== null) {
+            content = $(page.content).text()
+            var preview = content.split(" ", 20).join(" ")
+            $(`#search_result_${id}`).append(`<div class="text-muted">${preview}</div>`)
+        }
+
     })
 }
